00afca22e5c39f44715edef187dd45a0fc679140
Cung Tran <cung.n.tran@gmail.com>
2017-06-04 14:01:49 -0700

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -1,6 +1,5 @@
 <?php $pageTitle = "Create Task"; ?>
 <?php include_once "partials/header.php" ?>
-
 <div class="container-fluid">
     <section class="col-xs-12 main">
        <div id="ajax_msg" class=" col-xs-6 pull-right alert alert-success"></div>
@@ -8,8 +7,7 @@
         <table class="table table-striped table-bordered table-responsive">
             <thead>
             <tr><th>Name</th><th>Description</th><th>Status</th><th>Created</th><th>Action</th></tr>
-            </thead>
-           
+            </thead> 
             <tbody id="task-list"></tbody>
         </table>
     </section>

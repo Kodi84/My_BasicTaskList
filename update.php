103849b5a965094c7a11fa738c91696e05eee2e9
Cung Tran <cung.n.tran@gmail.com>
2017-06-04 14:01:33 -0700

    Update update.php

--- update.php
+++ update.php
@@ -4,18 +4,14 @@ include_once "database.php";
 if(isset($_POST['name']) && isset($_POST['id'])){
     $name = trim($_POST['name']);
     $id = $_POST['id'];
-    
     try{
         //build query
         $updateQuery = "UPDATE tasks SET name = :name 
-                        Where id = :id";  
-        
+                        Where id = :id";    
         //prepare query
-        $statement = $conn->prepare($updateQuery);
-        
+        $statement = $conn->prepare($updateQuery);      
         //execute the statement
-        $statement->execute(array(":name"=>$name,":id"=>$id));
-        
+        $statement->execute(array(":name"=>$name,":id"=>$id));  
         if($statement->rowCount()===1){
             echo "Succefully Updated Name Record";
         }else{
@@ -25,23 +21,18 @@ if(isset($_POST['name']) && isset($_POST['id'])){
         echo ("Error has occured".$e->getMessage());
     }
 }
-
 //update Description
 if(isset($_POST['description']) && isset($_POST['id'])){
     $description = trim($_POST['description']);
-    $id = $_POST['id'];
-    
+    $id = $_POST['id'];   
     try{
         //build query
         $updateQuery = "UPDATE tasks SET description = :description 
-                        Where id = :id";  
-        
+                        Where id = :id";    
         //prepare query
-        $statement = $conn->prepare($updateQuery);
-        
+        $statement = $conn->prepare($updateQuery); 
         //execute the statement
-        $statement->execute(array(":description"=>$description,":id"=>$id));
-        
+        $statement->execute(array(":description"=>$description,":id"=>$id));   
         if($statement->rowCount()===1){
             echo "Succefully Updated Name Record";
         }else{
@@ -51,23 +42,18 @@ if(isset($_POST['description']) && isset($_POST['id'])){
         echo ("Error has occured".$e->getMessage());
     }
 }
-
 //update Status
 if(isset($_POST['status']) && isset($_POST['id'])){
     $status = trim($_POST['status']);
-    $id = $_POST['id'];
-    
+    $id = $_POST['id'];   
     try{
         //build query
         $updateQuery = "UPDATE tasks SET status = :status 
-                        Where id = :id";  
-        
+                        Where id = :id";       
         //prepare query
-        $statement = $conn->prepare($updateQuery);
-        
+        $statement = $conn->prepare($updateQuery); 
         //execute the statement
-        $statement->execute(array(":status"=>$status,":id"=>$id));
-        
+        $statement->execute(array(":status"=>$status,":id"=>$id));   
         if($statement->rowCount()===1){
             echo "Succefully Updated Name Record";
         }else{
@@ -77,5 +63,4 @@ if(isset($_POST['status']) && isset($_POST['id'])){
         echo ("Error has occured".$e->getMessage());
     }
 }
-
 ?>
